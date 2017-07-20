96982281374b22d3924ca5538c5c09aab2555ac4
David Rolfe <rolf0090@umn.edu>
2017-07-20 00:26:32 -0500

    Initial Commit

---
+++ test_package/test.py
@@ -0,0 +1,16 @@
+'''
+Created on Jul 20, 2017
+
+@author: David Rolfe
+'''
+
+class Test(object):
+    '''
+    Test class for me to work with to get used to working with python again 
+    '''
+    
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
